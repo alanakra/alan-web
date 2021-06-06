be5ab5096253fba9a1d076036b9f082e58961d7a
Alan <alanakra@hotmail.fr>
2021-06-06 23:55:03 +0200

    Add footer comp

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+
+function Footer(props) {
+ return (
+  <div className="fixed bottom-0 dark:bg-black">
+   <div className="flex justify-between w-full">
+    <p>Designed with ❤ by Alan Akra</p>
+
+    <div>
+     <ul>
+      <a href="https://github.com/alanakra">GitHub</a>
+     </ul>
+    </div>
+    
+   </div>
+  </div>
+ );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Title from '../components/Title'
 import Navbar from '../components/Navbar'
 import Image from 'next/image'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -20,6 +21,8 @@ export default function Home() {
       <p className="text-xl">My name is <a href="mailto:bonjour@alanakra.fr">Alan Akra</a> and i'm student in a vocational degree of web development💻. I'm currently Web developer intern at <a href="https://www.soprabanking.com" target="_blank">SBS</a>. <br/>
       I'm interested about Web technologies such as React ⚛ and creative development 🖼, but also mobile development with <a href="https://developer.apple.com/swift/" target="_blank">Swift 📱</a> who i discovered recently.</p>
       <p className="text-xl">You can find me on <a href="https://github.com/alanakra">GitHub</a> and <a href="https://www.linkedin.com/in/alanakra/">Linkedin</a>.</p>
+
+      <Footer/>
     </div>
   )
 }
