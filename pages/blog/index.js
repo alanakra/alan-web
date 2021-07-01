fe86d5e9a8cd10bb61ee7c5d8577683a716448ed
Alan <alanakra@hotmail.fr>
2021-07-01 23:48:27 +0200

    Up blog page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,20 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 function blog({posts}) {
 
  return (
-  <div>
-   <h1 className="font-bold">Hello</h1>
+  <div className="pd-screen py-4 h-all">
+    <Head>
+      <title>Alan Akra | Blog</title>
+    </Head>
+
+   <h1 className="important-title-page">Blog</h1>
    {
     posts.nodes.map(post => {
      return(
       <ul key={post.slug}>
-        <li>
+        <li className="text-xl">
           <Link href={`/blog/${post.slug}`}>{post.title}</Link>
         </li>
       </ul>
