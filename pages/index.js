c5f4b33cc3a907ba4eb8a584c3307b0c28fecb83
Alan <alanakra@hotmail.fr>
2021-11-16 11:54:14 +0100

    Remove IMM cursus & Spelling error

--- pages/index.js
+++ pages/index.js
@@ -19,19 +19,13 @@ function About() {
   }]
 
   const education = [{
-    title: 'Master of Web & Mobile engineering',
-    school: '@IIM',
-    startDate: 'Oct 2021',
-    endDate: 'June 2022',
-    link: 'http://iim.fr'
-  },{
     title: 'Bachelor Web developer',
     school: '@CYU',
     startDate: 'Sept 2020',
     endDate: 'Jun 2021',
     link: 'http://lp-dw.fr'
   },{
-    title: '2 year University degree in Multimedia and Internet ',
+    title: '2 years University degree in Multimedia and Internet ',
     school: '@UnivGustaveEiffel',
     startDate: 'Sept 2018',
     endDate: 'Apr 2020',
@@ -61,7 +55,7 @@ function About() {
       }
     `}</style>
 
-    <h1 className="important-title-page">About me</h1>
+    <h1 className="important-title-page gradient-text">About me</h1>
 
     <div className="flex justify-center mb-5">
       <img
@@ -73,7 +67,8 @@ function About() {
       />
     </div>
 
-    <p className="text-xl mb-4">Hi 👋 I’m <a href="https://www.linkedin.com/in/alanakra/" className="link-color">Alan Akra</a>, a fullstack web developer based in the Greater Paris. I’m a emphatic person and I love code and manage digital projects. I also like Bo Bun and Phở 🍜. Currently <a href="http://cyu.fr" className="link-color">@CYU</a> on <a href="http://lp-dw.fr" className="link-color">Bachelor Web Development</a> , i’m joining a <a href="https://www.iim.fr/cursus/mastere-ingenierie-web-mobile/" className="link-color">Master of Web & Mobile</a> engineering <a href="http://iim.fr" className="link-color">@IIM</a> in September and I’m looking opportunities for an apprenticeship in web development.</p>
+    <p className="text-xl mb-4">Hi 👋 I’m <a href="https://www.linkedin.com/in/alanakra/" className="link-color">Alan Akra</a>, a fullstack web developer based in the Greater Paris. I’m a emphatic person and I love code and manage digital projects. I also like Bo Bun and Phở 🍜. Recently graduated from <a href="http://cyu.fr" className="link-color">@CYU</a> on <a href="http://lp-dw.fr" className="link-color">Bachelor Web Development</a> , i’m looking opportunities for a permanent contract in web development.
+I am also available for <a href="https://www.malt.fr/profile/alanakra" className="link-color">freelance</a> assignments.</p>
 
     <p className="text-xl mb-6">You can reach me with my <a href="mailto:alanakra@hotmail.fr" className="link-color">email</a>, my <a href="/cv.pdf" className="link-color">CV</a> (in french) and my <a href="https://github.com/alanakra" className="link-color">GitHub</a>.</p>
 
