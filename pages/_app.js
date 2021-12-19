6a99f2e2fa36aa46a9865f9849426589e76f3b35
Alan <alanakra@hotmail.fr>
2021-12-20 00:15:29 +0100

    Theme color on black

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import '../styles/styles.css'
 import Navbar from '../components/Navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="theme-color" content="#000000"/>
+      </Head>
       <Navbar/>
       <Component {...pageProps} />
     </>
