6af93124671fc497091be30fb7469c25b58a8f86
Alan <alanakra@hotmail.fr>
2021-12-20 16:21:42 +0100

    Use window for sessionStorage

--- components/Banner.js
+++ components/Banner.js
@@ -1,20 +1,24 @@
-import { useEffect } from 'react';
 import Link from 'next/link'
 
 export default function Banner(props) {
+ console.log(props)
 
  const hideBanner = () => {
-  useEffect(()=>{
+  if (typeof window !== 'undefined') {
     sessionStorage.setItem('hideBanner', true)
-  },[])
+  }
+ }
+
+ if (props.warn == 'true') {    
+  return null;
  }
  
  return (
-  <div className="bg-black text-white sm:text-center p-2 flex justify-between">
+  <div className="bg-black text-white sm:text-center p-2 flex justify-between cursor-pointer">
    <div></div>
    <p>I’m looking opportunities for a permanent contract in web development. I am also available for <a href="https://www.malt.fr/profile/alanakra" className="link-color">freelance</a> assignments. My <Link href="/CV.pdf"><a className="link-color">CV</a></Link> is available here.</p>
 
-   <i class="fas fa-times" onClick={hideBanner}></i>
+   <i className="fas fa-times" onClick={hideBanner}></i>
   </div>
  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,22 @@ import '../styles/styles.css'
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  
+  if (typeof window !== 'undefined') {
+      const isBannerClosed = sessionStorage.getItem('hideBanner')
+      console.log(isBannerClosed)
+  }
+
   return (
     <>
       <Head>
         <meta name="theme-color" content="#000000"/>
         <script async src="https://kit.fontawesome.com/997660e778.js" crossOrigin="anonymous"></script>
       </Head>
-      <Banner/>
+      <Banner warn={'isBannerClosed'}/>
       <Navbar/>
       <Component {...pageProps} />
     </>
