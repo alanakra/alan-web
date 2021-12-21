adbf4cbc2de7b3594296717cae5b910ff2b8c597
Alan <alanakra@hotmail.fr>
2021-12-21 18:14:29 +0100

    Remove flex class

--- components/Banner.js
+++ components/Banner.js
@@ -2,8 +2,7 @@ import Link from 'next/link'
 
 export default function Banner(props) {
  return (
-  <div className="bg-black text-white sm:text-center p-2 flex justify-between cursor-pointer">
-   <div></div>
+  <div className="bg-black text-white sm:text-center p-2 cursor-pointer">
    <p>I’m looking opportunities for a permanent contract in web development. I am also available for <a href="https://www.malt.fr/profile/alanakra" className="link-banner">freelance</a> assignments. My <Link href="/CV.pdf"><a className="link-banner">CV</a></Link> is available here.</p>
   </div>
  );
